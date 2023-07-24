1f0023e1a7f224a5507405375f23d45d4073be5d
Divineshia Sharon <divine@Divineshias-Air.lan>
2023-07-24 15:36:27 -0500

    selected featured puppy

--- PuppyPal/src/App.jsx
+++ PuppyPal/src/App.jsx
@@ -4,14 +4,27 @@ import './App.css'
 
 function App() {
   const [puppies,setPuppies]= useState(puppyList)
+  const [featPupId, setfeatPupId]= useState(null);
   console.log(puppyList) //repeating twice on console bec of strict mode in main.jsx
-
+  const featuredPup = puppies.find((pup)=> pup.id===featPupId)
+  console.log(featuredPup);
   return (
       <div className='App'>{
         puppies.map((puppy)=> {
-          return <p key ={puppy.id}>{puppy.name}</p>
+          return (<p onClick={()=>{
+          setfeatPupId(puppy.id)
+          }} key ={puppy.id}>{puppy.name}</p>)
         })
         }
+         {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+            </div>
+         )}
         </div>
     
   )
